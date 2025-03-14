6be459a9c628473fdf8587ea285410241515e612
Nathan Young <nya33@cs.sfu.ca>
2025-03-14 00:23:28 -0700

    Added function level examples

---
+++ vuln-0011/fixed-function/src/lib.rs
@@ -0,0 +1,218 @@
+extern crate byteorder;
+
+use std::{fmt, error, str};
+
+use byteorder::{BigEndian, ByteOrder};
+
+mod tables;
+
+/// Available encoding character sets
+#[derive(Clone, Copy, Debug)]
+pub enum CharacterSet {
+    /// The standard character set (uses `+` and `/`)
+    Standard,
+    /// The URL safe character set (uses `-` and `_`)
+    UrlSafe
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum LineEnding {
+    LF,
+    CRLF,
+}
+
+#[derive(Clone, Copy, Debug)]
+pub enum LineWrap {
+    NoWrap,
+    Wrap(usize, LineEnding)
+}
+
+/// Contains configuration parameters for base64 encoding
+#[derive(Clone, Copy, Debug)]
+pub struct Config {
+    /// Character set to use
+    char_set: CharacterSet,
+    /// True to pad output with `=` characters
+    pad: bool,
+    /// Remove whitespace before decoding, at the cost of an allocation
+    strip_whitespace: bool,
+    /// ADT signifying whether to linewrap output, and if so by how many characters and with what ending
+    line_wrap: LineWrap,
+}
+
+///Encode arbitrary octets as base64.
+///Returns a String.
+///
+///# Example
+///
+///```rust
+///extern crate base64;
+///
+///fn main() {
+///    let b64 = base64::encode_config(b"hello world~", base64::STANDARD);
+///    println!("{}", b64);
+///
+///    let b64_url = base64::encode_config(b"hello internet~", base64::URL_SAFE);
+///    println!("{}", b64_url);
+///}
+///```
+pub fn encode_config<T: ?Sized + AsRef<[u8]>>(input: &T, config: Config) -> String {
+    let mut buf = match encoded_size(input.as_ref().len(), config) {
+        Some(n) => String::with_capacity(n),
+        None => panic!("integer overflow when calculating buffer size")
+    };
+
+    encode_config_buf(input, config, &mut buf);
+    buf
+}
+
+
+// calculate the base64 encoded string size, including padding
+fn encoded_size(bytes_len: usize, config: Config) -> Option<usize> {
+    let printing_output_chars = bytes_len
+
+    .checked_add(2)
+        .map(|x| x / 3)
+        .and_then(|x| x.checked_mul(4));
+    //TODO this is subtly wrong but in a not dangerous way
+    //pushing patch with identical to previous behavior, then fixing
+    let line_ending_output_chars = match config.line_wrap {
+        LineWrap::NoWrap => Some(0),
+
+        LineWrap::Wrap(n, LineEnding::CRLF) =>
+            printing_output_chars.map(|y| y / n).and_then(|y| y.checked_mul(2)),
+
+        LineWrap::Wrap(n, LineEnding::LF) =>
+            printing_output_chars.map(|y| y / n),
+    };
+
+    printing_output_chars.and_then(|x|
+        line_ending_output_chars.and_then(|y| x.checked_add(y))
+    )
+
+}
+
+///Encode arbitrary octets as base64.
+///Writes into the supplied buffer to avoid allocations.
+///
+///# Example
+///
+///```rust
+///extern crate base64;
+///
+///fn main() {
+///    let mut buf = String::new();
+///    base64::encode_config_buf(b"hello world~", base64::STANDARD, &mut buf);
+///    println!("{}", buf);
+///
+///    buf.clear();
+///    base64::encode_config_buf(b"hello internet~", base64::URL_SAFE, &mut buf);
+///    println!("{}", buf);
+///}
+///```
+pub fn encode_config_buf<T: ?Sized + AsRef<[u8]>>(input: &T, config: Config, buf: &mut String) {
+    let input_bytes = input.as_ref();
+    let ref charset = match config.char_set {
+        CharacterSet::Standard => tables::STANDARD_ENCODE,
+        CharacterSet::UrlSafe => tables::URL_SAFE_ENCODE,
+    };
+
+    // reserve to make sure the memory we'll be writing to with unsafe is allocated
+    let resv_size = match encoded_size(input_bytes.len(), config) {
+        Some(n) => n,
+        None => panic!("integer overflow when calculating buffer size"),
+    };
+    buf.reserve(resv_size);
+
+    let orig_buf_len = buf.len();
+    let mut fast_loop_output_buf_len = orig_buf_len;
+
+    let input_chunk_len = 6;
+
+    let last_fast_index = input_bytes.len().saturating_sub(8);
+
+    // we're only going to insert valid utf8
+    let mut raw = unsafe { buf.as_mut_vec() };
+    // start at the first free part of the output buf
+    let mut output_ptr = unsafe { raw.as_mut_ptr().offset(orig_buf_len as isize) };
+    let mut input_index: usize = 0;
+    if input_bytes.len() >= 8 {
+        while input_index <= last_fast_index {
+            let input_chunk = BigEndian::read_u64(&input_bytes[input_index..(input_index + 8)]);
+
+            // strip off 6 bits at a time for the first 6 bytes
+            unsafe {
+                std::ptr::write(output_ptr, charset[((input_chunk >> 58) & 0x3F) as usize]);
+                std::ptr::write(output_ptr.offset(1), charset[((input_chunk >> 52) & 0x3F) as usize]);
+                std::ptr::write(output_ptr.offset(2), charset[((input_chunk >> 46) & 0x3F) as usize]);
+                std::ptr::write(output_ptr.offset(3), charset[((input_chunk >> 40) & 0x3F) as usize]);
+                std::ptr::write(output_ptr.offset(4), charset[((input_chunk >> 34) & 0x3F) as usize]);
+                std::ptr::write(output_ptr.offset(5), charset[((input_chunk >> 28) & 0x3F) as usize]);
+                std::ptr::write(output_ptr.offset(6), charset[((input_chunk >> 22) & 0x3F) as usize]);
+                std::ptr::write(output_ptr.offset(7), charset[((input_chunk >> 16) & 0x3F) as usize]);
+                output_ptr = output_ptr.offset(8);
+            }
+
+            input_index += input_chunk_len;
+            fast_loop_output_buf_len += 8;
+        }
+    }
+
+    unsafe {
+        // expand len to include the bytes we just wrote
+        raw.set_len(fast_loop_output_buf_len);
+    }
+
+    // encode the 0 to 7 bytes left after the fast loop
+
+    let rem = input_bytes.len() % 3;
+    let start_of_rem = input_bytes.len() - rem;
+
+    // start at the first index not handled by fast loop, which may be 0.
+    let mut leftover_index = input_index;
+
+    while leftover_index < start_of_rem {
+        raw.push(charset[(input_bytes[leftover_index] >> 2) as usize]);
+        raw.push(charset[((input_bytes[leftover_index] << 4 | input_bytes[leftover_index + 1] >> 4) & 0x3f) as usize]);
+        raw.push(charset[((input_bytes[leftover_index + 1] << 2 | input_bytes[leftover_index + 2] >> 6) & 0x3f) as usize]);
+        raw.push(charset[(input_bytes[leftover_index + 2] & 0x3f) as usize]);
+
+        leftover_index += 3;
+    }
+
+    if rem == 2 {
+        raw.push(charset[(input_bytes[start_of_rem] >> 2) as usize]);
+        raw.push(charset[((input_bytes[start_of_rem] << 4 | input_bytes[start_of_rem + 1] >> 4) & 0x3f) as usize]);
+        raw.push(charset[(input_bytes[start_of_rem + 1] << 2 & 0x3f) as usize]);
+    } else if rem == 1 {
+        raw.push(charset[(input_bytes[start_of_rem] >> 2) as usize]);
+        raw.push(charset[(input_bytes[start_of_rem] << 4 & 0x3f) as usize]);
+    }
+
+    if config.pad {
+        for _ in 0..((3 - rem) % 3) {
+            raw.push(0x3d);
+        }
+    }
+
+    //TODO FIXME this does the wrong thing for nonempty buffers
+    if orig_buf_len == 0 {
+        if let LineWrap::Wrap(line_size, line_end) = config.line_wrap {
+            let len = raw.len();
+            let mut i = 0;
+            let mut j = 0;
+
+            while i < len {
+                if i > 0 && i % line_size == 0 {
+                    match line_end {
+                        LineEnding::LF => { raw.insert(j, b'\n'); j += 1; }
+                        LineEnding::CRLF => { raw.insert(j, b'\r'); raw.insert(j + 1, b'\n'); j += 2; }
+                    }
+                }
+
+                i += 1;
+                j += 1;
+            }
+        }
+    }
+}
