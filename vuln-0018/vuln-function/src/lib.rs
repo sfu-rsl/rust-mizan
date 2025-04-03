7f11349a867f2c144d4af151c9e759924d30e43c
Tarek <tareknaser360@gmail.com>
2025-04-03 16:03:27 -0700

    vuln-0018 to vuln-0023: `CVE 2020 25791` to `CVE-2020-25796`
    Signed-off-by: Tarek <tareknaser360@gmail.com>

---
+++ vuln-0018/vuln-function/src/lib.rs
@@ -0,0 +1,46 @@
+mod types;
+
+use core::mem::MaybeUninit;
+
+use typenum::U64;
+
+use crate::types::ChunkLength;
+
+pub struct Chunk<A, N = U64>
+where
+    N: ChunkLength<A>,
+{
+    left: usize,
+    right: usize,
+    data: MaybeUninit<N::SizedType>,
+}
+
+impl<A, N> Chunk<A, N>
+where
+    N: ChunkLength<A>,
+{
+    /// Construct a new chunk with one item.
+    pub fn unit(value: A) -> Self {
+        let mut chunk = Self {
+            left: 0,
+            right: 1,
+            data: MaybeUninit::uninit(),
+        };
+        unsafe {
+            Chunk::force_write(0, value, &mut chunk);
+        }
+        chunk
+    }
+
+    /// Write a value at an index without trying to drop what's already there
+    #[inline]
+    unsafe fn force_write(index: usize, value: A, chunk: &mut Self) {
+        chunk.mut_ptr(index).write(value)
+    }
+
+    #[inline]
+    unsafe fn mut_ptr(&mut self, index: usize) -> *mut A {
+        (&mut self.data as *mut _ as *mut A).add(index)
+    }
+}
+
