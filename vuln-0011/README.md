f95cf3b3c06d554765d57422a1d08682d29d4087
Vincent Huang <vyh1@sfu.ca>
2025-05-26 13:44:18 -0700

    feat(vuln-0011): add vuln to dataset, update Cargo.toml and mizan.json

---
+++ vuln-0011/README.md
@@ -0,0 +1,27 @@
+# Vulnerability: RUSTSEC-2025-0019
+
+| **Information**       | **Details**                                                                                     |
+| --------------------- | ----------------------------------------------------------------------------------------------- |
+| **CVE**               | [RUSTSEC-2025-0019](https://rustsec.org/advisories/RUSTSEC-2025-0019.html)                         |
+| **Vulnerable Commit** | [1cf18d1](https://github.com/planus-org/planus/commit/1cf18d16af7cf0b17c8f95f7c0fd362c69c78236) |
+| **Fixed Commit**      | [be6f99a](https://github.com/planus-org/planus/commit/be6f99afde8760dcf87b5dcdade832400e826791) |
+| **Variants**          | - [fixed-crate](fixed-crate)                                                                    |
+|                       | - [fixed-function](fixed-function)                                                              |
+|                       | - [vuln-crate](vuln-crate)                                                                      |
+|                       | - [vuln-function](vuln-function)                                                                |
+
+### Vulnerable lines
+
+`src/lib.rs`
+
+```rust
+fn write_impl(&mut self, value: [T; N]) {
+    *self.slice = unsafe {
+        /// VULNERABILITY: The Drop implementation will get run twice when using the cursor
+        let ptr = &value as *const [T; N] as *const [MaybeUninit<T>; N];
+        let read_value = core::ptr::read(ptr);
+        core::mem::drop(value);
+        read_value
+    };
+}
+```
---
+++ vuln-0011/vuln-function/src/lib.rs
@@ -0,0 +1,68 @@
+//! Small crate for initializing an uninitialized slice
+#![no_std]
+
+use core::mem::MaybeUninit;
+
+#[repr(C)]
+struct Wrapper<T, const Y: usize, const Z: usize>([T; Y], [T; Z]);
+
+const fn check_impl<T, const X: usize, const Y: usize, const Z: usize>() {
+    // Check that the array sizes match
+    assert!(X == Y + Z, "Array cannot be split: sizes don't match");
+    assert!(
+        usize::checked_add(Y, Z).is_some(),
+        "Array cannot be split: length would overflow"
+    );
+    // Make doubly sure that nothing funky is going on with the memory representations
+    assert!(core::mem::size_of::<Wrapper<T, Y, Z>>() == core::mem::size_of::<[T; X]>());
+    assert!(core::mem::align_of::<Wrapper<T, Y, Z>>() == core::mem::align_of::<[T; X]>());
+}
+
+trait SizeCheck<T, const X: usize, const Y: usize, const Z: usize> {
+    const CHECK: Self;
+}
+
+impl<T, const X: usize, const Y: usize, const Z: usize> SizeCheck<T, X, Y, Z> for () {
+    const CHECK: () = check_impl::<T, X, Y, Z>();
+}
+
+#[allow(clippy::let_unit_value)]
+fn check<T, const X: usize, const Y: usize, const Z: usize>() {
+    let _: () = SizeCheck::<T, X, Y, Z>::CHECK;
+    // Do the same checks at run-time, so even if rustc changes to allow ignore
+    // our compile-time errors, we will at least not create UB
+    check_impl::<T, X, Y, Z>();
+}
+
+pub(crate) fn split_mut<'a, T, const X: usize, const Y: usize, const Z: usize>(
+    x: &'a mut [T; X],
+) -> (&'a mut [T; Y], &'a mut [T; Z]) {
+    check::<T, X, Y, Z>();
+
+    let wrapper: &'a mut Wrapper<T, Y, Z> = unsafe { core::mem::transmute(x) };
+    (&mut wrapper.0, &mut wrapper.1)
+}
+
+/// A fixed-size cursor for initializing [`MaybeUninit`] arrays
+///
+/// The cursor will guarantee that all values have been
+/// initialized when the value is dropped, which means
+/// that it is safe to call [`MaybeUninit::assume_init()`].
+///
+/// **NOTE:** This guarantee only holds as long as [`Drop::drop()`] is called.
+///           If the value goes out of scope without drop being called (e.g. because
+///           of [`core::mem::forget()`]), then this guarantee no longer applies.
+pub struct Cursor<'a, T, const N: usize> {
+    slice: &'a mut [MaybeUninit<T>; N],
+}
+
+impl<'a, T, const N: usize> Cursor<'a, T, N> {
+    fn write_impl(&mut self, value: [T; N]) {
+        *self.slice = unsafe {
+            let ptr = &value as *const [T; N] as *const [MaybeUninit<T>; N];
+            let read_value = core::ptr::read(ptr);
+            core::mem::drop(value);
+            read_value
+        };
+    }
+}
