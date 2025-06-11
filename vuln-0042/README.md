93fa91becc1e690059c1077ec707c3f576040cb4
Vincent Huang <vyh1@sfu.ca>
2025-06-11 16:08:02 -0700

    chore(vuln-0042): add fixed function code sample

--- vuln-0042/README.md
+++ vuln-0042/README.md
@@ -6,10 +6,8 @@
 | **Vulnerable Commit** | [52cc113](https://github.com/Alexhuszagh/fast-float-rust/commit/52cc1131fbf28a6bd3a0cce3310c3864bb88f501) |
 | **Fixed Commit**      | [31d1abf](https://github.com/Alexhuszagh/fast-float-rust/commit/31d1abf7c6232f35eb069c64b8290e6fa92802b6) |
 | **Variants**          | - [fixed-crate](fixed-crate)                                                                   |
-|                       | - [fixed-file](fixed-file)                                                                     |
 |                       | - [fixed-function](fixed-function)                                                             |
 |                       | - [vuln-crate](vuln-crate)                                                                     |
-|                       | - [vuln-file](vuln-file)                                                                       |
 |                       | - [vuln-function](vuln-function)                                                               |
 
 ### Vulnerable Lines
---
+++ vuln-0042/fixed-function/src/lib.rs
@@ -0,0 +1,96 @@
+//! This crate provides a super-fast decimal number parser from strings into
+//! floats.
+//!
+//! ## Usage
+//!
+//! There's two top-level functions provided: [`parse`](crate::parse()) and
+//! [`parse_partial`](crate::parse_partial()), both taking
+//! either a string or a bytes slice and parsing the input into either `f32` or
+//! `f64`:
+//!
+//! - [`parse`](crate::parse()) treats the whole string as a decimal number and
+//!   returns an error if there are invalid characters or if the string is
+//!   empty.
+//! - [`parse_partial`](crate::parse_partial()) tries to find the longest
+//!   substring at the beginning of the given input string that can be parsed as
+//!   a decimal number and, in the case of success, returns the parsed value
+//!   along the number of characters processed; an error is returned if the
+//!   string doesn't start with a decimal number or if it is empty. This
+//!   function is most useful as a building block when constructing more complex
+//!   parsers, or when parsing streams of data.
+//!
+//! ## Examples
+//!
+//! ```rust
+//! // Parse the entire string as a decimal number.
+//! let s = "1.23e-02";
+//! let x: f32 = fast_float2::parse(s).unwrap();
+//! assert_eq!(x, 0.0123);
+//!
+//! // Parse as many characters as possible as a decimal number.
+//! let s = "1.23e-02foo";
+//! let (x, n) = fast_float2::parse_partial::<f32, _>(s).unwrap();
+//! assert_eq!(x, 0.0123);
+//! assert_eq!(n, 8);
+//! assert_eq!(&s[n..], "foo");
+//! ```
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![allow(unused_unsafe)]
+#![warn(unsafe_op_in_unsafe_fn)]
+#![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
+#![deny(
+    clippy::doc_markdown,
+    clippy::unnecessary_safety_comment,
+    clippy::semicolon_if_nothing_returned,
+    clippy::unwrap_used,
+    clippy::as_underscore,
+    clippy::doc_markdown
+)]
+#![allow(
+    clippy::cast_possible_truncation,
+    clippy::cast_possible_wrap,
+    clippy::cast_sign_loss,
+    clippy::cast_lossless,
+    clippy::cast_precision_loss,
+    clippy::missing_const_for_fn,
+    clippy::use_self,
+    clippy::module_name_repetitions,
+    clippy::cargo_common_metadata,
+    clippy::struct_field_names
+)]
+
+use std::marker::PhantomData;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct AsciiStr<'a> {
+    ptr: *const u8,
+    end: *const u8,
+    _marker: PhantomData<&'a [u8]>,
+}
+
+impl<'a> AsciiStr<'a> {
+    #[inline]
+    pub fn is_empty(&self) -> bool {
+        self.ptr == self.end
+    }
+
+    /// # Safety
+    ///
+    /// Safe if `!self.is_empty()`
+    #[inline]
+    pub unsafe fn first_unchecked(&self) -> u8 {
+        debug_assert!(!self.is_empty(), "attempting to get first value of empty buffer.");
+        unsafe { *self.ptr }
+    }
+
+    #[inline]
+    pub fn first(&self) -> Option<u8> {
+        if self.is_empty() {
+            None
+        } else {
+            // SAFETY: safe since `!self.is_empty()`
+            Some(unsafe { self.first_unchecked() })
+        }
+    }
+}
