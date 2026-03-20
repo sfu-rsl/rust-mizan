c6f573bfcddf13aa2ec370f4e1b61a0005d2a1dd
Specyy <121744367+Specyy@users.noreply.github.com>
2026-03-20 04:03:54 -0700

    fix(mizan-mut/explicit-return): Remove redundant tokens from return expression and correctly re-apply attributes from inner expressions

--- mizan-mut/src/mutations/explicit_return.rs
+++ mizan-mut/src/mutations/explicit_return.rs
@@ -1,9 +1,7 @@
 use anyhow::Result;
 use quote::quote;
 use syn::{
-    parse_file,
-    visit_mut::{self, VisitMut},
-    Stmt, ReturnType, ItemFn, Block, ImplItemFn, ExprBlock, Expr
+    Attribute, Block, ImplItemFn, ItemFn, Stmt, TraitItemFn, parse_file, parse_quote, visit_mut::{self, VisitMut}
 };
 
 pub struct ExplicitReturnMutator;
@@ -17,94 +15,86 @@ impl ExplicitReturnMutator {
     }
 }
 
+struct AttributeCheckVisitor {
+    attrs: Vec<Attribute>,
+    entered_expr: bool,
+}
+
+impl AttributeCheckVisitor {
+    fn new() -> Self {
+        Self {
+            attrs: vec![],
+            entered_expr: false,
+        }
+    }
+}
+
+impl VisitMut for AttributeCheckVisitor {
+    fn visit_stmt_mut(&mut self, _: &mut syn::Stmt) {}
+
+    fn visit_expr_mut(&mut self, e: &mut syn::Expr) {
+        // We only want to collect attributes at the current level
+        if self.entered_expr {
+            return;
+        }
+
+        self.entered_expr = true;
+        visit_mut::visit_expr_mut(self, e);
+    }
+
+    fn visit_attributes_mut(&mut self, attrs: &mut Vec<syn::Attribute>) {
+        std::mem::swap(&mut self.attrs, attrs);
+    }
+}
+
 struct ExplicitReturnVisitor;
+
+impl ExplicitReturnVisitor {
+    fn apply_return_mut(block: &mut Block) {
+        let stmts = &mut block.stmts;
+
+        if stmts.is_empty() {
+            return;
+        }
+
+        let mut last_stmt = stmts.last_mut().unwrap();
+
+        // None ensures there is no semi-colon
+        if let Stmt::Expr(ref mut expr, None) = &mut last_stmt {
+            // Collect the attributes since we can't wrap them in return expressions.
+            // We'll re-apply them manually down below.
+            let mut attr_visitor = AttributeCheckVisitor::new();
+            attr_visitor.visit_expr_mut(expr);
+
+            let attrs = attr_visitor.attrs;
+
+            let ret_expr = parse_quote! {
+                #(#attrs)*
+                return #expr;
+            };
+
+            *last_stmt = ret_expr;
+        }
+    }
+}
 
 impl VisitMut for ExplicitReturnVisitor {
     fn visit_impl_item_fn_mut(&mut self, item: &mut ImplItemFn) {
-        visit_mut::visit_visibility_mut(self, &mut item.vis);
-        visit_mut::visit_signature_mut(self, &mut item.sig);
-        visit_mut::visit_block_mut(self, &mut item.block);
-
-        // Skip functions returning ()
-        if matches!(item.sig.output, ReturnType::Default) {
-            return;
-        }
-
-        let stmts: &mut Vec<Stmt> = &mut item.block.stmts;
-
-        if stmts.is_empty() {
-            return;
-        }
-
-        let stmts_len = stmts.len();
-        let last_stmt: &mut Stmt = &mut stmts[stmts_len - 1];
-
-        match last_stmt {
-            Stmt::Expr(Expr::Block(b), semi) => {
-                // Only apply if last statement doesn't end in semi-colon
-                if semi.is_none() && b.attrs.is_empty() {
-                    let ret_expr_tokens = quote! {
-                        return {#b};
-                    };
-
-                    if let Ok(ret_expr) = syn::parse2(ret_expr_tokens) {
-                        *last_stmt = ret_expr;
-                    }
-                }
-            }
-            Stmt::Expr(expr, semi) => {
-                // Only apply if last statement doesn't end in semi-colon
-                if semi.is_none() {
-                    let ret_expr_tokens = quote! {
-                        return {#expr};
-                    };
-
-                    if let Ok(ret_expr) = syn::parse2(ret_expr_tokens) {
-                        *last_stmt = ret_expr;
-                    }
-                }
-            }
-            _ => {}
-        }
+        visit_mut::visit_impl_item_fn_mut(self, item);
+        Self::apply_return_mut(&mut item.block);
     }
 
     fn visit_item_fn_mut(&mut self, item: &mut ItemFn) {
-        visit_mut::visit_visibility_mut(self, &mut item.vis);
-        // visit_mut::visit_attributes_mut(self, &mut item.attrs);
-        visit_mut::visit_signature_mut(self, &mut item.sig);
-        visit_mut::visit_block_mut(self, &mut item.block);
-
-        // Skip functions returning ()
-        if matches!(item.sig.output, ReturnType::Default) {
-            return;
-        }
-
-        let stmts : &mut Vec<Stmt> = &mut item.block.stmts;
-
-        if stmts.is_empty() {
-            visit_mut::visit_item_fn_mut(self, item);
-            return;
-        }
-
-        let stmts_len = stmts.len();
-        let last_stmt : &mut Stmt = &mut stmts[stmts_len-1];
-
-        match last_stmt {
-            Stmt::Expr(Expr::Block(b), semi) => {
-                // Only apply if last statement doesn't end in semi-colon
-                if semi.is_none() && b.attrs.is_empty() {
-                    let ret_expr_tokens = quote! {
-                        return {#b};
-                    };
-
-                    if let Ok(ret_expr) = syn::parse2(ret_expr_tokens) {
-                        *last_stmt = ret_expr;
-                    }
-                }
-            }
-            _ => {}
-        }
-
+        visit_mut::visit_item_fn_mut(self, item);
+        Self::apply_return_mut(&mut item.block);
     }
 
+    fn visit_trait_item_fn_mut(&mut self, item: &mut TraitItemFn) {
+        visit_mut::visit_trait_item_fn_mut(self, item);
+
+        // Apply them to traits with default impl's as well
+        if let Some(block) = &mut item.default {
+            Self::apply_return_mut(block);
+        }
+    }
 }
